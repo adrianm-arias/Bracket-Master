5d5a39af22abcd2bfe0618354cd7c2c802fdd963
Adrian Arias <adrianarias.pro@gmail.com>
2022-09-23 00:36:07 +0000

    completed issue-2-user-can-view-each-individual-group.

--- client/components/second-nav.jsx
+++ client/components/second-nav.jsx
@@ -4,16 +4,16 @@ import parseRoute from '../lib/parse-route';
 export default function GroupsNav(props) {
   const route = parseRoute(window.location.hash);
   return (
-    <div className="second-nav-bg d-flex justify-content-center py-3 overflow-x">
-      <div className='container-xxl d-flex justify-content-between'>
-        <a className={`${route.params.get('group') === 'A' ? 'group-link active' : 'group-link'}`} aria-current="page" href="#groups?group=A">Group A</a>
-        <a className={`${route.params.get('group') === 'B' ? 'group-link active' : 'group-link'}`} aria-current="page" href='#groups?group=B'>Group B</a>
-        <a className={`${route.params.get('group') === 'C' ? 'group-link active' : 'group-link'}`} aria-current="page" href="#groups?group=C">Group C</a>
-        <a className={`${route.params.get('group') === 'D' ? 'group-link active' : 'group-link'}`} aria-current="page" href="#groups?group=D">Group D</a>
-        <a className={`${route.params.get('group') === 'E' ? 'group-link active' : 'group-link'}`} aria-current="page" href="#groups?group=E">Group E</a>
-        <a className={`${route.params.get('group') === 'F' ? 'group-link active' : 'group-link'}`} aria-current="page" href="#groups?group=F">Group F</a>
-        <a className={`${route.params.get('group') === 'G' ? 'group-link active' : 'group-link'}`} aria-current="page" href="#groups?group=G">Group G</a>
-        <a className={`${route.params.get('group') === 'H' ? 'group-link active' : 'group-link'}`} aria-current="page" href="#groups?group=H">Group H</a>
+    <div className='py-2 overflow-x'>
+      <div className='container-xxl d-flex justify-content-start'>
+        <a className={`${route.params.get('group') === 'A' ? 'group-link mx-2 active' : 'group-link mx-2'}`} aria-current='page' href='#groups?group=A'>Group A</a>
+        <a className={`${route.params.get('group') === 'B' ? 'group-link mx-2 active' : 'group-link mx-2'}`} aria-current='page' href='#groups?group=B'>Group B</a>
+        <a className={`${route.params.get('group') === 'C' ? 'group-link mx-2 active' : 'group-link mx-2'}`} aria-current='page' href='#groups?group=C'>Group C</a>
+        <a className={`${route.params.get('group') === 'D' ? 'group-link mx-2 active' : 'group-link mx-2'}`} aria-current='page' href='#groups?group=D'>Group D</a>
+        <a className={`${route.params.get('group') === 'E' ? 'group-link mx-2 active' : 'group-link mx-2'}`} aria-current='page' href='#groups?group=E'>Group E</a>
+        <a className={`${route.params.get('group') === 'F' ? 'group-link mx-2 active' : 'group-link mx-2'}`} aria-current='page' href='#groups?group=F'>Group F</a>
+        <a className={`${route.params.get('group') === 'G' ? 'group-link mx-2 active' : 'group-link mx-2'}`} aria-current='page' href='#groups?group=G'>Group G</a>
+        <a className={`${route.params.get('group') === 'H' ? 'group-link mx-2 active' : 'group-link mx-2'}`} aria-current='page' href='#groups?group=H'>Group H</a>
       </div>
     </div>
 
--- client/pages/groups.jsx
+++ client/pages/groups.jsx
@@ -5,55 +5,72 @@ import AppContext from '../lib/app-context';
 
 export default class Groups extends React.Component {
 
+  renderTeams(letter) {
+    const { teams } = this.context;
+    const group = [];
+    for (let i = 0; i < teams.length; i++) {
+      if (teams[i].group === letter) {
+        group.push(teams[i]);
+      }
+    }
+    const groupRender = group.map(groupList => {
+      return (
+        <a className='team-link' key={groupList.teamId} href={`#teams/teamid=${groupList.teamId}`}>
+          <div className='team-wrapper my-2 mx-auto d-flex justify-content-start'>
+            <img className='team-flag me-4' src={groupList.countryFlag} alt="" />
+            <h1 className='team-name'>{groupList.countryName}</h1>
+          </div>
+        </a>
+      );
+    });
+
+    return (
+      <div className='mx-2'>{groupRender}</div>
+    );
+  }
+
   renderGroup() {
     const route = parseRoute(window.location.hash);
     if (route.params.get('group') === 'A') {
-      return (
-        <h1>Group A</h1>
-      );
+      return 'A';
     }
     if (route.params.get('group') === 'B') {
-      return (
-        <h1>Group B</h1>
-      );
+      return 'B';
     }
     if (route.params.get('group') === 'C') {
-      return (
-        <h1>Group C</h1>
-      );
+      return 'C';
     }
     if (route.params.get('group') === 'D') {
-      return (
-        <h1>Group D</h1>
-      );
+      return 'D';
     }
     if (route.params.get('group') === 'E') {
-      return (
-        <h1>Group E</h1>
-      );
+      return 'E';
     }
     if (route.params.get('group') === 'F') {
-      return (
-        <h1>Group F</h1>
-      );
+      return 'F';
     }
     if (route.params.get('group') === 'G') {
-      return (
-        <h1>Group G</h1>
-      );
+      return 'G';
     }
     if (route.params.get('group') === 'H') {
-      return (
-        <h1>Group H</h1>
-      );
+      return 'H';
     }
   }
 
   render() {
+    const groupClicked = this.renderGroup();
     return (
       <>
-        <GroupsNav />
-        { this.renderGroup() }
+        <div className='second-nav-bg d-flex justify-content-center'>
+          <GroupsNav />
+        </div>
+        <h1 className='pt-5 text-center mx-auto mb-2 page-title'>{`Group ${groupClicked}`}</h1>
+        <div className='d-md-flex justify-content-center my-4'>
+          {this.renderTeams(groupClicked)}
+        </div>
+        <div className='d-flex justify-content-center'>
+          <button className='btn btn-main'>Start New Prediction</button>
+        </div>
       </>
     );
   }
