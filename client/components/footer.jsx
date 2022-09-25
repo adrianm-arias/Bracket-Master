76cf9f393e0957a064d4d93597ddd75666dab2a9
Adrian Arias <adrianarias.pro@gmail.com>
2022-09-25 00:19:15 +0000

    in progress

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import parseRoute from '../lib/parse-route';
 
-export default function Footer(props) {
-  const route = parseRoute(window.location.hash);
-  return (
-    <footer className='navbar fixed-bottom navbar-expand-lg navbar-dark nav-bg py-2'>
-      <div className='container-xxl d-flex flex-column'>
-        <div className='pb-3'>
-          <a href='#home'><i className={`${route.path === 'home' ? 'bi bi-house-door footer-icon-xs active' : 'bi bi-house-door footer-icon-xs'}`} /></a>
+export default class Footer extends React.Component {
 
-          <a href='#bracket'><i className={`${route.path === 'bracket' ? 'bi bi-layout-three-columns footer-icon-xs active' : 'bi bi-layout-three-columns footer-icon-xs'}`} /></a>
+  render() {
+    const route = parseRoute(window.location.hash);
 
-          <a href='#groups?group=A'><i className={`${route.path === 'groups' ? 'bi bi-grid footer-icon-xs active' : 'bi bi-grid footer-icon-xs'}`} /></a>
+    return (
+      <footer className='navbar fixed-bottom navbar-expand-lg navbar-dark nav-bg py-2'>
+        <div className='container-xxl d-flex flex-column'>
+          <div className='pb-3'>
+            <a href='#home'><i className={`${route.path === 'home' ? 'bi bi-house-door footer-icon-xs active' : 'bi bi-house-door footer-icon-xs'}`} /></a>
 
-          <a href='#teams'><i className={`${route.path === 'teams' ? 'bi bi-flag footer-icon-xs active' : 'bi bi-flag footer-icon-xs'}`} /></a>
+            <a href='#bracket'><i className={`${route.path === 'bracket' ? 'bi bi-layout-three-columns footer-icon-xs active' : 'bi bi-layout-three-columns footer-icon-xs'}`} /></a>
+
+            <a href='#groups?group=A'><i className={`${route.path === 'groups' ? 'bi bi-grid footer-icon-xs active' : 'bi bi-grid footer-icon-xs'}`} /></a>
+
+            <a href='#teams'><i className={`${route.path === 'teams' ? 'bi bi-flag footer-icon-xs active' : 'bi bi-flag footer-icon-xs'}`} /></a>
+          </div>
+          <div className='justify-content-center'>
+            <p className='cr-text'>
+              Copyright © 2022. All rights reserved.
+            </p>
+          </div>
         </div>
-        <div className='justify-content-center'>
-          <p className='cr-text'>
-            Copyright © 2022. All rights reserved.
-          </p>
-        </div>
-      </div>
-    </footer>
-  );
+      </footer>
+    );
+  }
 }
--- client/pages/groups.jsx
+++ client/pages/groups.jsx
@@ -8,65 +8,100 @@ export default class Groups extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      teamId: 1
+      isSelected: false,
+      isEditing: false,
+      teamId: 1,
+      groupStage: [
+        {
+          a1: null
+        }
+      ]
     };
   }
 
   renderTeams(letter) {
     const { teams } = this.context;
+
+    // checks which group needs to be rendered
     const group = [];
     for (let i = 0; i < teams.length; i++) {
       if (teams[i].group === letter) {
         group.push(teams[i]);
       }
     }
+    // Render the team info
     const groupRender = group.map(groupList => {
       const teamId = groupList.teamId - 1;
       const StringId = teamId.toString();
       return (
-        <div className="accordion accordion-flush" id="accordionFlushExample" key={groupList.teamId}>
-          <div className="accordion-item">
-            <h2 className="accordion-header" id={`flush-heading-${StringId}`}>
-              <button className="empty-btn collapsed" type="button" data-bs-toggle="collapse" data-bs-target={`#collapse-${StringId}`} aria-expanded="false" aria-controls={`collapse-${StringId}`}>
-                <div className='team-wrapper my-1 mx-auto d-flex justify-content-start'>
-                  <img className='team-flag me-4' src={groupList.countryFlag} alt={`${groupList.countryName} Flag`} />
-                  <h1 className='team-name'>{groupList.countryName}</h1>
+        (this.state.isEditing)
+          ? <button type="button" className='empty-btn' key={groupList.teamId} onClick={() => this.setState({ groupStage: [{ a1: groupList.teamId }] })} >
+            <div className='team-wrapper selected my-1 mx-auto d-flex justify-content-start'>
+              <img className='team-flag me-4' src={groupList.countryFlag} alt={`${groupList.countryFlag}-flag`} />
+              <h1 className='team-name'>{groupList.countryName}</h1>
+            </div>
+          </button>
+          : <div className='accordion accordion-flush' id='accordionFlushExample' key={groupList.teamId}>
+            <div className='accordion-item'>
+              <h2 className='accordion-header' id={`flush-heading-${StringId}`}>
+                <button className='empty-btn collapsed' type='button' data-bs-toggle='collapse' data-bs-target={`#collapse-${StringId}`} aria-expanded='false' aria-controls={`collapse-${StringId}`}>
+                  <div className='team-wrapper my-1 mx-auto d-flex justify-content-start'>
+                    <img className='team-flag me-4' src={groupList.countryFlag} alt={`${groupList.countryName} Flag`} />
+                    <h1 className='team-name'>{groupList.countryName}</h1>
+                  </div>
+                </button>
+              </h2>
+              <div id={`collapse-${StringId}`} className='accordion-collapse collapse' aria-labelledby={`flush-heading-${StringId}`} >
+                <div className='accordion-body'>
+                  <p className='fs-5'>Coach: <span className='fw-semibold'>{teams[teamId].coach}</span></p>
+                  <p className='fs-5'>Rank: <span className='fw-semibold'>{teams[teamId].fifaRank}</span></p>
+                  <p className='fs-5'>Last 5 Games:</p>
+                  <div className='d-flex flex-row mb-4'>
+                    <div className={`${teams[teamId].fifthGame} match-icon d-flex justify-content-center align-items-center`}>
+                      <p className='fs-6 match-icon-text'>{teams[teamId].fifthGame}</p>
+                    </div>
+                    <div className={`${teams[teamId].fourthGame} match-icon d-flex justify-content-center align-items-center`}>
+                      <p className='fs-6 match-icon-text'>{teams[teamId].fourthGame}</p>
+                    </div>
+                    <div className={`${teams[teamId].thirdGame} match-icon d-flex justify-content-center align-items-center`}>
+                      <p className='fs-6 match-icon-text'>{teams[teamId].thirdGame}</p>
+                    </div>
+                    <div className={`${teams[teamId].secondGame} match-icon d-flex justify-content-center align-items-center`}>
+                      <p className='fs-6 match-icon-text'>{teams[teamId].secondGame}</p>
+                    </div>
+                    <div className={`${teams[teamId].firstGame} match-icon d-flex justify-content-center align-items-center`}>
+                      <p className='fs-6 match-icon-text'>{teams[teamId].firstGame}</p>
+                    </div>
+                  </div>
+                  <p className='fs-5'>2018 World Cup Finish: <span className='fw-semibold'>{teams[teamId].prevWC}</span></p>
+                  <a className='fs-5 fw-semibold fa-link' target='_blank' rel='noreferrer' href={teams[teamId].websiteLink}>Visit Football Association Website <i className='bi bi-chevron-right' />
+                  </a>
                 </div>
-              </button>
-            </h2>
-            <div id={`collapse-${StringId}`} className="accordion-collapse collapse" aria-labelledby={`flush-heading-${StringId}`} >
-              <div className="accordion-body">
-                <p className='fs-5'>Coach: <span className='fw-semibold'>{teams[teamId].coach}</span></p>
-                <p className='fs-5'>Rank: <span className='fw-semibold'>{teams[teamId].fifaRank}</span></p>
-                <p className='fs-5'>Last 5 Games:</p>
-                <div className='d-flex flex-row mb-4'>
-                  <div className={`${teams[teamId].fifthGame} match-icon d-flex justify-content-center align-items-center`}>
-                    <p className='fs-6 match-icon-text'>{teams[teamId].fifthGame}</p>
-                  </div>
-                  <div className={`${teams[teamId].fourthGame} match-icon d-flex justify-content-center align-items-center`}>
-                    <p className='fs-6 match-icon-text'>{teams[teamId].fourthGame}</p>
-                  </div>
-                  <div className={`${teams[teamId].thirdGame} match-icon d-flex justify-content-center align-items-center`}>
-                    <p className='fs-6 match-icon-text'>{teams[teamId].thirdGame}</p>
-                  </div>
-                  <div className={`${teams[teamId].secondGame} match-icon d-flex justify-content-center align-items-center`}>
-                    <p className='fs-6 match-icon-text'>{teams[teamId].secondGame}</p>
-                  </div>
-                  <div className={`${teams[teamId].firstGame} match-icon d-flex justify-content-center align-items-center`}>
-                    <p className='fs-6 match-icon-text'>{teams[teamId].firstGame}</p>
-                  </div>
-                </div>
-                <p className='fs-5'>2018 World Cup Finish: <span className='fw-semibold'>{teams[teamId].prevWC}</span></p>
-                <a className='fs-5 fw-semibold fa-link' target="_blank" rel="noreferrer" href={teams[teamId].websiteLink}>Visit Football Association Website <i className="bi bi-chevron-right" />
-                </a>
               </div>
             </div>
           </div>
-        </div>
       );
     });
     return (
       <div className='mx-2'>{groupRender}</div>
+    );
+  }
+
+  handleEditing() {
+    return (
+      <div className='editing-wrapper d-flex justify-content-between'>
+        <div className='mt-2'>
+          <p className=''>Currently Editing: <span className='editing-wrapper-title'>Bracket 1</span></p>
+        </div>
+        <div className='mt-1 d-flex'>
+          <button className='empty-btn'>
+            <i className='bi bi-check-circle-fill editing-icons px-3' />
+          </button>
+          <button className='empty-btn'>
+            <i onClick={() => this.setState({ isEditing: null })} className='bi bi-dash-circle-fill editing-icons px-3' />
+          </button>
+        </div>
+      </div>
     );
   }
 
@@ -99,12 +134,13 @@ export default class Groups extends React.Component {
   }
 
   render() {
-
-    // const teamId = this.state.teamId;
+    // const { teams } = this.context;
+    // console.log('groups render:', teams);
+    // console.log('groups render:', this.state.groupStage[0]);
+    // console.log('isEditing State:', this.state.isEditing);
     const groupClicked = this.renderGroup();
     return (
       <>
-
         {/* {this.state.openModal && this.handleModal()} */}
         <div className='second-nav-bg d-flex justify-content-center'>
           <GroupsNav />
@@ -115,10 +151,15 @@ export default class Groups extends React.Component {
             {this.renderTeams(groupClicked)}
           </div>
         </div>
-        <div className='d-flex justify-content-center'>
-          <button className='btn btn-main'>Start New Prediction</button>
+        {(!this.state.isEditing)
+          ? <div className='d-flex justify-content-center'>
+            <button onClick={() => this.setState({ isEditing: [] })} className='btn btn-primary'>Start New Prediction</button>
+          </div>
+          : null
+        }
+        <div className='fixed-bottom'>
+          { this.state.isEditing && this.handleEditing()}
         </div>
-
       </>
     );
   }
